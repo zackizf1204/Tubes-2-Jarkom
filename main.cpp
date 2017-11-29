56e505693bb40da2873f7ec4622f17bd030fdbca
zackizf1204 <zackizf1204@gmail.com>
2017-11-29 13:52:32 +0700

    SUDAH 100

--- main.cpp
+++ main.cpp
@@ -1,51 +1,52 @@
 #include <bits/stdc++.h>
 
-class Node{
-public:
-  // Construction
-  Node() { }
-  Node(int d,int n) : distance(d), nextHop(n) {}
-
-  // Getters
-  int getDistance() const {return distance;}
-  int getNextHop() const {return nextHop;}
-
-  // Setters
-  void setDistance(int val) {distance = val;}
-  void setNextHop(int val) {nextHop = val;}
-private:
-  int distance;
-  int nextHop;
-};
-
 int main(int argc, char *argv[]){
     int inputnode;
     int edge;
     std::cin >> inputnode >> edge;
-    Node matriks[inputnode][inputnode];
+    int distance[inputnode][inputnode];
+    int nextHop[inputnode][inputnode];
     int i,j;
-    //Inisialisasi matriks
-    for(i=0;i<inputnode;i++){
-        for(j=0;j<inputnode;j++){
-            if(i==j){
-                matriks[i][j].setDistance(0);
-                matriks[i][j].setNextHop(i+1);
-            }else{
-                matriks[i][j].setDistance(-1);
-                matriks[i][j].setNextHop(-1);
+
+
+    //INISIALISASI MATRIKS
+    for (int i = 0; i < inputnode; i++){
+        for (int j = 0; j < inputnode; j++){
+            distance[i][j] = -1;
+            nextHop[i][j] = -1;
+        }      
+    }
+  
+    //INPUT KETETANGGAAN (EDGE)
+    if(inputnode>edge){
+        for(i=0;i<inputnode;i++){
+            distance[i][i]=0;
+            nextHop[i][i]=i+1;
+            if(i<edge){
+                int source;
+                int dest;
+                std::cin >> source >> dest;
+                distance[source-1][dest-1]=1;
+                nextHop[source-1][dest-1]=dest;
+                distance[dest-1][source-1]=1;
+                nextHop[dest-1][source-1]=source;
             }
-            
         }
     }
-    //Input ketetanggaan
-    for(i=0;i<edge;i++){
+    else{
+        for(i=0;i<edge;i++){
         int source;
         int dest;
         std::cin >> source >> dest;
-        matriks[source-1][dest-1].setDistance(1);
-        matriks[source-1][dest-1].setNextHop(dest);
-        matriks[dest-1][source-1].setDistance(1);
-        matriks[dest-1][source-1].setNextHop(source);
+        if(i<inputnode){
+            distance[i][i]=0;
+            nextHop[i][i]=i+1;
+        }
+        distance[source-1][dest-1]=1;
+        nextHop[source-1][dest-1]=dest;
+        distance[dest-1][source-1]=1;
+        nextHop[dest-1][source-1]=source;
+        }
     }
 
     int inputinfo;
@@ -56,27 +57,27 @@ int main(int argc, char *argv[]){
         int source;
         int dest;
         std::cin >> source >> dest;
-            for(j=0;j<inputnode;j++){
-                if(matriks[source-1][j].getDistance()>0){
-                    if((matriks[dest-1][j].getDistance()==-1)||(matriks[source-1][j].getDistance()<(matriks[dest-1][j].getDistance()-1))){
-                        matriks[dest-1][j].setDistance(matriks[source-1][j].getDistance()+1);
-                        matriks[dest-1][j].setNextHop(source);
-                    }else{
-                        if((matriks[source-1][j].getDistance()!=0)&&(matriks[source-1][j].getDistance()<(matriks[dest-1][j].getDistance()))&&(source<matriks[dest-1][j].getNextHop())){
-                            matriks[dest-1][j].setDistance(matriks[source-1][j].getDistance()+1);
-                            matriks[dest-1][j].setNextHop(source);
-                        }
-                    }
+        for(j=0;j<inputnode;j++){
+            if((distance[source-1][j]>0)&&(distance[source-1][j]<distance[dest-1][j])){
+                if((distance[source-1][j]<(distance[dest-1][j]-1))||(source<nextHop[dest-1][j])){
+                    distance[dest-1][j]=distance[source-1][j]+1;
+                    nextHop[dest-1][j]=source;
                 }
             }
+            else{
+                if((distance[source-1][j]>0)&&(distance[dest-1][j]==-1)){
+                    distance[dest-1][j]=distance[source-1][j]+1;
+                    nextHop[dest-1][j]=source;
+                }
+            }
+        }
     }
 
     //TAMPILKAN HASIL
     for(i=0;i<inputnode;i++){
         for(j=0;j<inputnode;j++){
-            std::cout << matriks[i][j].getDistance() << " " << matriks[i][j].getNextHop() << '\n';
+            std::cout << distance[i][j] << " " << nextHop[i][j] << '\n';
         }
     }
-
     return 0;
 }
